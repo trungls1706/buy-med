3120e66e6f50bcb69f404dc8f16d6a19174376e4
trungls1706 <trungls1706@gmail.com>
2026-02-01 20:19:46 +0700

    init src

---
+++ app/_layout.tsx
@@ -0,0 +1,15 @@
+import { Stack } from 'expo-router';
+import 'react-native-reanimated';
+
+export const unstable_settings = {
+  anchor: '(tabs)',
+};
+
+export default function RootLayout() {
+
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text } from 'react-native';
+
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function QuickOrderScreen() {
+
+
+    return (
+        <SafeAreaView className="flex-1 bg-[#F8FAFB]" edges={['top', 'left', 'right']}>
+            <Text>Quick Order</Text>
+        </SafeAreaView>
+    );
+}
